ada1f44f93693bfe4f98168ad681fbacb00b1edf
flozanoisla <flozanoisla@gmail.com>
2025-09-02 15:53:57 -0500

    clase practica

---
+++ practica.R
@@ -0,0 +1,48 @@
+# Mi primerar práctica con R
+
+# alt + -
+
+x <- "Hola"
+y <- "UNTRM"
+
+# f(x, y) = x + x^2 + 2xy
+
+# ayuda: F1 | Fn + F1
+
+paste(x, y)
+
+paste(x, y, sep = " + ")
+
+paste(X, y)
+
+?paste
+
+# tidyverse cran
+
+library(tidyverse)
+
+# pipe
+
+a1 <- "estudiante"
+1a <- "cocina"
+
+a_1 <- "objeto"
+a.1 <- "a"
+a-1 <- "b"
+
+
+notas <- c(16, 17, 19, 15)
+media <- mean(notas)
+media
+
+# ctrl + shift + m => %>% 
+
+m <- c(16, 17, 19, 15) %>% 
+  mean()
+m
+
+
+
+
+
+
